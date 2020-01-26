b83195d6391167218520801b47f06580f1c22283
elvis <elvisilde@gmail.com>
2020-01-26 14:07:13 +0100

    Divide community matrix and centrality matrix, add community comparison

--- script.R
+++ script.R
@@ -4,12 +4,15 @@ library(centiserve)
 ##
 ## Import the graph
 ##
-g <- read.graph("karate.gml", format="gml")
+g <- read.graph("~/Documents/UNI/SEM3/Network Analysis/network-analysis/karate.gml", format="gml")
 
 # Print the initial graph
 V(g)$color <- sample( c("red", "black"), vcount(g), rep=TRUE)
 E(g)$color <- "grey"
 plot(g)
+
+# Ground truth
+ground_truth = V(g)$value
 
 ##
 ## CENTRALITIES
@@ -24,40 +27,41 @@ centr5 <- closeness.currentflow(g)
 centr6 <- subgraph_centrality(g)
 #centr7 <- sna::flowbet(get.edgelist(g, names=FALSE)) 
 
+# put together the centralities in a matrix
+centrMatrix <- do.call(rbind, Map(data.frame, C1 = centr1, C2 = centr2,
+                                  C3 = centr3, C4 = centr4, C5 = centr5, C6 = centr6))
+print("Centrality matrix")
+print(centrMatrix)
+
 ##
 ## COMMUNITIES
 ## Calculate communities using different algorithms
 ##
 com1 <- multilevel.community(g)
-print ( compare(com1, V(g)$value, method="nmi" ) )
-
 com2 <- edge.betweenness.community(g)
-print ( compare(com2, V(g)$value, method="nmi" ) )
-
 com3 <- walktrap.community(g)
-print ( compare(com3, V(g)$value, method="nmi" ) )
-
 com4 <- infomap.community(g)
-print ( compare(com4, V(g)$value, method="nmi" ) )
 
 # Print the community graph
 # plot(g, vertex.color=membership(com4))
 
-##
-## BUILD THE MATRIX
-## For each node we have the evaluation using
-## each community detection algorithm and the ground truth at the end
-##
-
-# ground truth
-ground_truth = V(g)$value + 1
-
-# put together the centralities and memberships in a matrix
-matrix <- do.call(rbind, Map(data.frame, C1 = centr1, C2 = centr2,
-                             C3 = centr3, C4 = centr4, C5 = centr5, C6 = centr6,
-                             #C7 = centr7,
-                             A=membership(com1), B=membership(com2), 
-                             C=membership(com3), D=membership(com4), Ground=ground_truth))
+# put together all community calculated memberships in a matrix
+commMatrix <- do.call(rbind, Map(data.frame, 
+                                 A=membership(com1), B=membership(com2), 
+                                 C=membership(com3), D=membership(com4), Ground=ground_truth))
 
 # print the matrix
-print(matrix)
+print("Community matrix")
+print(commMatrix)
+
+##
+## Compare each community detection algorithm to the ground truth using NMI and choose the best one
+##
+print("NMI comparison")
+l <- rbind(c("A: ", compare(com1, ground_truth, method="nmi" )), c("B: ", compare(com2, ground_truth, method="nmi" )),
+                  c("C: ", compare(com3, ground_truth, method="nmi" )), c("D: ", compare(com4, ground_truth, method="nmi" )))
+print(l)
+
+print("Best community algorithm")
+best_algo <- l[which.max(l[,2]),]
+print(best_algo)
